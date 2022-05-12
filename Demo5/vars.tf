fc9947b07c359f35ff15183b3d64f5e4e2946009
Ubuntu <ubuntu@ip-172-31-18-140.us-west-2.compute.internal>
2022-05-12 06:51:12 +0000

    added backend file

---
+++ Demo5/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-09d56f8956ab235b3"
+    us-west-2 = "ami-0ee8244746ec5d6d4"
+    eu-west-1 = "ami-00c90dbdc12232b58"
+  }
+}
